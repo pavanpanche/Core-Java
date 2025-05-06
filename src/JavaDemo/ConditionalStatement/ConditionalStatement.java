3d9ac45115701c7aa9b944944ba745eacce94043
pavanpanche <pavanpanche2@gmail.com>
2025-05-06 17:23:19 +0530

    frist Commit

---
+++ src/JavaDemo/ConditionalStatement/ConditionalStatement.java
@@ -0,0 +1,18 @@
+package JavaDemo.ConditionalStatement;
+
+public class ConditionalStatement {
+
+
+    int a;
+    int  b;
+    public static void main (String[] args) {
+        int a =10;
+        int b= 22;
+
+        if (a > b) {
+            System.out.println("Physics marks is high");
+        }else {
+            System.out.println("Chemistry marks is high");
+        }
+    }
+}
---
+++ src/JavaDemo/ConditionalStatement/StatementDemo.java
@@ -0,0 +1,18 @@
+package JavaDemo.ConditionalStatement;
+
+public class StatementDemo {
+    public static void main (String[] args ){
+        int a = 10;   // input statement
+
+        System.out.println(a);  //Output Statement
+
+        //Simple if statement
+
+        if (a<11){
+            System.out.println("java!");
+        }
+
+    }
+
+
+}
