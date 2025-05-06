ee8fc6fa7fb8f6af750367dcd846059a431ca751
Pavan Kumar Panche <pavanpanche2@gmail.com>
2025-05-06 14:29:44 +0530

    file added

---
+++ JavaBasicProgram.java
@@ -0,0 +1,16 @@
+package JavaDemo;
+
+public class JavaBasicProgram {
+
+    public static void main(String[] args) {
+        System.out.println(5+6); // we can perform Basic operation inside print statement.
+         // output is 11
+        System.out.println("pavan " +" panche"); // we can add String also
+        // output is pavan panche
+    }
+}
+
+
+
+
+
